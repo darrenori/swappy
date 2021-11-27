630338627b58d999cbdef27c1e8513635f6e14f6
darrenori <87930795+darrenori@users.noreply.github.com>
2021-11-28 01:27:18 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -35,7 +35,26 @@ $router->get('/signup?error=none', 'signup.php');
 //signup inc route handling
 $router->post('/incsignup', 'includes/signup.inc.php');
 
-//signup inc route handling
+//pre verification route handling
+$router->get('/emailverification', 'phpmailer/emailotp.php');
+$router->get('/emailverification?error=badotp', 'phpmailer/emailotp.php');
+
+//pre verification inc route handling
+$router->post('/emailverificationinc', 'includes/emailotp.inc.php');
+
+//google auth route handling
+$router->get('/googleauthentication', 'googleauth/googleauthotp.php');
+$router->get('/googleauthentication?error=badotp', 'googleauth/googleauthotp.php');
+
+//google auth inc route handling
+$router->post('/googleauthenticationinc', 'includes/googleauth.inc.php');
+
+//post login route handling
 $router->get('/campus', 'campus.php');
+$router->post('/logout', 'includes/logout.inc.php');
+$router->get('/logout', 'includes/logout.inc.php');
+
+
+$router->post('/check','includes/user_auth.php');
 
 
