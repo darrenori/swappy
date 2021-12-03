d9a434f8d9f1ce9a7efbe1f8af8b2bd5882a198a
darrenori <87930795+darrenori@users.noreply.github.com>
2021-12-03 17:06:52 +0800

    Add files via upload

---
+++ tasks/addtasks.php
@@ -0,0 +1,74 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['loginstate'])) {
+        header("location: https://www.swapamc.com/swapproj/login");
+        exit();
+    } elseif ($_SESSION['loginstate'] === "A") {
+        header("location: https://www.swapamc.com/swapproj/emailverification");
+        exit();
+    } elseif ($_SESSION['loginstate'] === "B") {
+        header("location: https://www.swapamc.com/swapproj/googleauthentication");
+        exit();
+    } elseif (!$_SESSION['loginstate'] === "OK") {
+        header("location: https://www.swapamc.com/swapproj/logout");
+        exit();
+    }
+    
+    
+        
+    require_once $_SERVER['DOCUMENT_ROOT']. '/swapproj/includes/dbh.inc.php';
+    require_once $_SERVER['DOCUMENT_ROOT']. '/swapproj/manager/includes/employee.inc.php';
+    $fname = $_SESSION['employeefname'];
+    
+    $userid = $_SESSION['userid'];
+    $role = $_SESSION['role'];
+    if($role==6||$role==5||$role==3){
+        
+    } else {
+        echo "ur a fake";
+    }
+
+    echo "<form action='/swapproj/employeemanager/taskmanager/addtaskinc' method=POST>";
+    echo "Task name"."<br>";
+    echo "<input type='text' name='taskname'>"."<br>";
+
+    echo "Task details"."<br>";
+    echo "<input type='text' name='taskdetails'>"."<br>";
+
+    
+
+
+
+    echo "Date to Finish by"."<br>";
+    echo "<input type='datetime-local'  id='date' name='date'>";
+
+    echo "<br><br>";
+
+    echo "Assigned to:"."<br>";
+    echo $_SESSION['employeefname']."<br>";
+
+
+    echo "Assigned by:"."<br>";
+    echo $_SESSION['username']."<br>";
+    
+    echo "<br><br>";
+    echo "<input type='submit'>";
+    
+    echo "</form>";
+
+
+    if(isset($_GET['error'])){
+        if($_GET['error']='baddate'){
+            echo "INVALID DATE";
+        }
+    }
+?>
+
+<script>
+    var today = new Date().toISOString();
+    today = today.substring(0, today.length - 8);
+
+    console.log(today)
+    document.getElementById("date").min = today;
+</script>
---
+++ tasks/includes/edittasks.inc.php
@@ -0,0 +1,68 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['loginstate'])) {
+    header("location: https://www.swapamc.com/swapproj/login");
+    exit();
+} elseif ($_SESSION['loginstate'] === "A") {
+    header("location: https://www.swapamc.com/swapproj/emailverification");
+    exit();
+} elseif ($_SESSION['loginstate'] === "B") {
+    header("location: https://www.swapamc.com/swapproj/googleauthentication");
+    exit();
+} elseif (!$_SESSION['loginstate'] === "OK") {
+    header("location: https://www.swapamc.com/swapproj/logout");
+    exit();
+}
+
+require_once $_SERVER['DOCUMENT_ROOT']. '/swapproj/includes/dbh.inc.php';
+require_once $_SERVER['DOCUMENT_ROOT']. '/swapproj/tasks/includes/tasks.inc.php';
+
+
+if(isset($_POST['name'])&&isset($_POST['details'])&&isset($_POST['progress'])&&isset($_POST['assignedby'])){
+    $name = $_POST['name'];
+    $details = $_POST['details'];
+    $progress = $_POST['progress'];
+    $assignedby = $_POST['assignedby'];
+
+    if($progress=='Received'){
+        $progress=0;
+    } elseif($progress=='In Progress'){
+        $progress=1;
+    } elseif($progress=='Waiting for check'){
+        $progress=2;
+    } else {
+        //kick
+    }
+
+    
+
+    if(badInput([$name,$details,$progress,$assignedby])==0){
+        
+    } else {
+        //kick em
+    }
+
+    
+}
+
+$taskid = $_SESSION['task'];
+$employeeid = $_SESSION['employeeid'];
+
+date_default_timezone_set('Asia/Singapore');
+$now = time();
+$now = date('Y-m-d', $now)." ".date('H:i:s');
+
+$query = $conn->prepare("UPDATE mydb.employees_task SET task_name = '$name', task_details = '$details',
+task_progress='$progress',task_assignedby='$assignedby',task_dateedited='$now' WHERE task_id = '$taskid';");
+
+
+if($query->execute()){
+    header("location: https://www.swapamc.com/swapproj/employeemanager/taskmanager?user=$employeeid");
+
+}
+
+
+
+?>
