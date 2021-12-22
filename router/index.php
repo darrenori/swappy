7caccb7f175e70f63a84ec5e75f9cb14ea8b4128
veryniceHub <92866426+veryniceHub@users.noreply.github.com>
2021-12-22 22:03:23 +0800

    Update index.php

--- router/index.php
+++ router/index.php
@@ -84,20 +84,44 @@ class Router {
         $uri = $_SERVER['REQUEST_URI'];
 
         
-        // echo $uri;
-        // echo '<br>';
-        // echo $route;
-        // echo '<br>';
+        if(!strpos($route,"?")){ 
+            if($route!="/swapproj/"){ 
 
-        if ($uri === $route) {
+                //use the $route parameter and replace "/" for regex to read properly. 
+                $regexroute = str_replace('/','(\/)',$route);
+        
+                /*
+                
+                /  \i        -> is to check for pattern
+                             -> we are checking GET parameter
+                             -> for example ?ans=five in "../wordhere?ans=five" 
+                
+                */
+                $regexroute ="/". $regexroute . "(\?)(.*)(\=)(.*)/i";
+        
+                //Uncomment these to commands to debug
+                // echo $regexroute;
+                // echo "<br>";
+                // echo $uri;
+                // echo "<br>";
+            
+        
+                $value = preg_match($regexroute,$uri);
+
+
+                //Uncomment these to commands to debug
+                // echo $value;
+                // echo "<br><br><br>";
+            }
+           
+        }   
+
+
+        if ( ($uri === $route) || ($uri == $route . "/" && $uri!='/swapproj//') || $value == 1 ) {
             $this->handled = true;
             return include_once (views . $view);
         }
     }
-
-
-
-    
 
 
     /**
