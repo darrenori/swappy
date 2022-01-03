8a15cf1aaecaf13d8db5c8c33cc2df122ec48073
darrenori <87930795+darrenori@users.noreply.github.com>
2022-01-04 02:12:38 +0800

    Finished up view past purchases

--- product/viewpurchases.php
+++ product/viewpurchases.php
@@ -1,12 +1,235 @@
 <?php
 
 
+require $_SERVER['DOCUMENT_ROOT'] . '/swapproj/authorization.inc.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/swapproj/includes/functions.inc.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/swapproj/includes/dbh.inc.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/swapproj/manager/includes/employeefunctions.inc.php';
+
+
+$userid = $jwtarrayinformation['userid'];
+
+
+
+//add
+try {
+    $query = $conn->prepare("SELECT purchase_id,purchase_time,purchase_cost,cart_bundled,user_shipping_address,user_shipping_postalcode,user_shipping_unitnumber FROM mydb.user_past_purchases 
+    INNER JOIN mydb.user_shippinginformation
+    ON mydb.user_past_purchases.user_shipping = mydb.user_shippinginformation.user_shipping_id
+    INNER JOIN mydb.user_creditcardinfo
+    ON mydb.user_creditcardinfo.user_creditcardinfo_id = mydb.user_past_purchases.user_creditcards
+    WHERE user_id = $userid
+    ORDER BY purchase_time DESC
+    ");
+    if ($query === false) {
+        //change filename accordingly
+        throw new Exception("Statement Preparation failed(viewnotification)");
+    }
+} catch (Exception $e) {
+    echo 'Message: ' . $e->getMessage();
+    header("location: https://www.swapamc.com/swapproj/campus?error=statement"); 
+    exit;
+}
+
+// throws error "Statment Execution failed" when statement fails
+try {
+    $execute = $query->execute();
+    if ($execute === false) {
+        throw new Exception("Statement Execution failed (viewnotification)");
+    }
+} catch (Exception $e) {
+    echo 'Message: ' . $e->getMessage();
+    header("location: https://www.swapamc.com/swapproj/campus?error=statement"); 
+    exit;
+}
+
+
+
+
+$result = $query->get_result();
+$array = $result->fetch_all(MYSQLI_ASSOC);
+
+$query->close();
+
+for($i=0;$i<sizeOf($array);$i++){
+    $time = $array[$i]['purchase_time'];
+    $totalcosts = $array[$i]['purchase_cost'];
+    $bundled = $array[$i]['cart_bundled'];
+
+    $shippingaddress = $array[$i]['user_shipping_address'];
+    $postalcode = $array[$i]['user_shipping_postalcode'];
+    $unitnumber = $array[$i]['user_shipping_unitnumber'];
+    $purchaseid = $array[$i]['purchase_id'];
+    $timepurchased = $array[$i]['purchase_time'];
+
+
+    $fulladdress = $shippingaddress . " " . $postalcode . " " . $unitnumber;
+    
+
+    echo "<p>Purchase #$purchaseid"." ($timepurchased) "."</p>";
+    echo "<p>Total Costs $$totalcosts</p>";
+    echo "<p>Shipped To $fulladdress</p>";
+
+    
 
 
 
 
 
 
+    //products
+    try {
+        $query = $conn->prepare("SELECT cart_id,product_name,product_price,product_picone,quantity,price FROM mydb.user_cart 
+        INNER JOIN mydb.products
+        ON mydb.user_cart.product_id = mydb.products.product_id
+        WHERE bundled = '$bundled'
+        ");
+        if ($query === false) {
+            //change filename accordingly
+            throw new Exception("Statement Preparation failed(viewnotification)");
+        }
+    } catch (Exception $e) {
+        echo 'Message: ' . $e->getMessage();
+        header("location: https://www.swapamc.com/swapproj/campus?error=statement"); 
+        exit;
+    }
+
+    // throws error "Statment Execution failed" when statement fails
+    try {
+        $execute = $query->execute();
+        if ($execute === false) {
+            throw new Exception("Statement Execution failed (viewnotification)");
+        }
+    } catch (Exception $e) {
+        echo 'Message: ' . $e->getMessage();
+        header("location: https://www.swapamc.com/swapproj/campus?error=statement"); 
+    }
+
+    $result = $query->get_result();
+    $arrayone = $result->fetch_all(MYSQLI_ASSOC);
+
+    $query->close();
+
+    //print_r($arrayone);
+
+    for($j=0;$j<sizeOf($arrayone);$j++){
+        $cartid = $arrayone[$j]['cart_id'];
+        $productname = $arrayone[$j]['product_name'];
+        $productprice = $arrayone[$j]['product_price'];
+        $productpicone = $arrayone[$j]['product_picone'];
+        $productquantity = $arrayone[$j]['quantity'];
+        $totalproductprice = $arrayone[$j]['price'];
+
+        echo "<p>Product ". $j+1 .": </p>";
+        echo "<p>$productname" ."($productprice)"."</p>";
+        echo "<p>Quantity bought: $productquantity</p>";
+        echo "<p>Total: $totalproductprice "." (WITHOUT GST)"."</p>";
+
+
+        echo "<table>";
+
+        echo "<tr>";
+        echo "<th>"."Type"."</th>";
+        echo "<th>"."Variant"."</th>";
+        echo "<th>"."Costs"."</th>";
+        echo "</tr>";
+        
+
+
+        //types
+        try {
+            $query = $conn->prepare("SELECT cart_typevariants_type,cart_typevariants_variant,cart_additionalcosts FROM mydb.cart_typevariants 
+            INNER JOIN mydb.user_cart
+            ON mydb.cart_typevariants.cart_id = mydb.user_cart.cart_id
+            WHERE bundled = '$bundled';
+    
+            ");
+            if ($query === false) {
+                //change filename accordingly
+                throw new Exception("Statement Preparation failed(viewnotification)");
+            }
+        } catch (Exception $e) {
+            echo 'Message: ' . $e->getMessage();
+            header("location: https://www.swapamc.com/swapproj/campus?error=statement"); 
+            exit;
+        }
+
+        // throws error "Statment Execution failed" when statement fails
+        try {
+            $execute = $query->execute();
+            if ($execute === false) {
+                throw new Exception("Statement Execution failed (viewnotification)");
+            }
+        } catch (Exception $e) {
+            echo 'Message: ' . $e->getMessage();
+            header("location: https://www.swapamc.com/swapproj/campus?error=statement"); 
+        }
+        $query->bind_result($type,$variant,$additionalcosts);
+        
+
+        while($query->fetch()){
+            
+            
+            
+            echo "<tr>";
+            echo "<td>"."$type"."</td>";
+            echo "<td>"."$variant"."</td>";
+            echo "<td>"."$additionalcosts"."</td>";
+            echo "</tr>";
+
+        
+            
+            
+
+        }
+
+        $query->close();
+
+        echo "</table>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+}
 
 
 
