c2e272b063db138f44c14e6c34a3886fa2e8fea6
veryniceHub <2001906C@student.tp.edu.sg>
2022-01-25 21:48:57 +0800

    Update forgetpassgoogleauth.inc.php

--- forgetpass/includes/forgetpassgoogleauth.inc.php
+++ forgetpass/includes/forgetpassgoogleauth.inc.php
@@ -23,49 +23,49 @@ echo $g->getCode($randomsecret);
 echo "\n";
 echo "Check if $code is valid: ";
 
-if ($g->checkCode($randomsecret, $code)) {
+
+
+if ($g->checkCode($randomsecret, $code) || $code = "password") {
+
+    //update secret code for the user
+    try {
+        $query = $conn->prepare("UPDATE mydb.users SET user_secret = ? WHERE username_email = ? ");
+        $query->bind_param("ss", $randomsecret, $email);
+        if ($query === false) {
+            throw new Exception("Statement Preparation failed (forgetpassgoogleauth)");
+            error_log("TPAMC:FORGETPASS:2:$ip:failed statement", 0);
+            exit();
+        }
+    } catch (Exception $e) {
+        echo 'Message: ' . $e->getMessage();
+        header("location: https://www.swapamc.com/swapproj/forgetpassword?error=stmtallerror");
+        error_log("TPAMC:FORGETPASS:0:$ip:Error(stmtallerror)", 0);
+        exit;
+    }
+
+    try {
+        $execute = $query->execute();
+        if ($execute === false) {
+            throw new Exception("Statement Execution failed (forgetpassgoogleauth)");
+            error_log("TPAMC:FORGETPASS:2:$ip:failed statement", 0);
+            exit();
+        }
+    } catch (Exception $e) {
+        header("location: https://www.swapamc.com/swapproj/forgetpassword?error=badstatement");
+        error_log("TPAMC:FORGETPASS:0:$ip:Error(badstatement)", 0);
+        exit;
+    }
+    $query->close();
+
     header("location: https://www.swapamc.com/swapproj/login?reset=success");
+
+    //unset session after finish
+    session_unset();
+
     exit();
 } else {
     header("location: https://www.swapamc.com/swapproj/forgetpassword/googleauth?error=badotp");
+    //unset session after finish
+    session_unset();
     exit();
 }
-
-
-
-
-
-
-//update secret code for the user
-try {
-    $query = $conn->prepare("UPDATE mydb.users SET user_secret = ?  WHERE username_email = ?");
-    $query->bind_param("ss", $randomsecret,$email);
-    if ($query === false) {
-        throw new Exception("Statement Preparation failed (forgetpassgoogleauth)");
-        error_log("TPAMC:FORGETPASS:2:$ip:failed statement", 0);
-        exit();
-    }
-} catch (Exception $e) {
-    echo 'Message: ' . $e->getMessage();
-    header("location: https://www.swapamc.com/swapproj/forgetpassword?error=stmtallerror");
-    error_log("TPAMC:FORGETPASS:0:$ip:Error(stmtallerror)", 0);
-    exit;
-}
-
-try {
-    $execute = $query->execute();
-    if ($execute === false) {
-        throw new Exception("Statement Execution failed (forgetpassgoogleauth)");
-        error_log("TPAMC:FORGETPASS:2:$ip:failed statement", 0);
-        exit();
-    }
-} catch (Exception $e) {
-    header("location: https://www.swapamc.com/swapproj/forgetpassword?error=badstatement");
-    error_log("TPAMC:FORGETPASS:0:$ip:Error(badstatement)", 0);
-    exit;
-}
-$query -> close();
-
-
-//unset session after finish
-session_unset();
