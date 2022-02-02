b1604a5d7b7be252a0884b2f4b5e738c9b9b6f6e
darrenori <87930795+darrenori@users.noreply.github.com>
2022-02-03 02:00:57 +0800

    prototype

---
+++ product/css/productcss.php
@@ -0,0 +1,620 @@
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@500&display=swap');
+
+*{
+    margin:0;
+    padding:0;
+    
+    color:white;
+}
+body {
+    background:black;
+    font-family:Montserrat;
+}
+
+html, body {
+max-width: 100%;
+overflow-x: hidden;
+}
+
+.top {
+    margin: 120px 45px;
+    
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-between;
+    /* width: 100%; */
+    /* background-color: rgba(255, 255, 255, 0.3); */
+}
+
+
+.left {
+    display: flex;
+    padding: 20px 0px;
+    flex-wrap: wrap;
+    flex-basis: 35%;    
+    /* background-color: gainsboro; */
+}
+
+.right {
+    flex-basis: 55%;
+    border-left: 2px solid white;
+    /* display: flex; */
+    flex-wrap: wrap;
+    margin:0;
+    padding: 20px 10px;
+    padding-left: 20px;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-direction: row;
+
+
+}
+
+
+.tags {
+    font-weight: bold;
+    margin-bottom: 20px;
+    margin-top: 10px;
+}
+
+.tagsheader {
+    flex-basis: 100%;
+    color: white;
+    margin-bottom: 10px;
+
+}
+
+.tagsbtn {
+    color: white;
+    border:0;
+    padding: 12px 65px;
+    background-color: #8D1D25;
+    margin-right: 20px;
+    border-radius: 3px;
+    font-weight: bold;
+}
+
+.extrainfo {
+    display: flex;
+    flex-wrap: wrap;
+    flex-basis: 100%;
+}
+
+.extrainforow {
+    font-weight: bold;
+    flex-basis: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-bottom: 25px;
+    font-size: 1em;
+    align-items: center;
+}
+
+.tk {
+    color:#8D1D25;
+    font-size: 2em;
+    
+}
+
+
+
+.right .tpamc {
+    color: #E26565;
+    flex-basis: 100%;
+    font-weight: 100;
+
+
+}
+
+
+.right .prodname {
+    flex-basis: 100%;
+}
+
+.right .price {
+    margin-top: 30px;
+    color: white;
+
+}
+
+.refunds,.details {
+    margin-top: 30px;
+    flex-basis: 100%;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.refundsheader,.detailsheader {
+    color: #E26565;
+    flex-basis: 100%;
+    font-weight: bold;
+}
+
+.refundsbody,.detailsbody {
+    color: white;
+    opacity: 0.8;
+}
+
+
+.typescontainer {
+    margin-top: 30px;
+}
+
+.typename {
+    font-weight: bold;
+    color: #E26565;
+}
+
+.typerow {
+    /* display: flex; */
+    flex-basis: 100%;
+    flex-wrap: wrap;
+    margin-bottom: 50px;
+}
+
+.variants {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: flex-start;
+}
+
+.donate-now {
+  list-style-type: none;
+
+  /* margin: 25px 0 0 0;
+  padding: 0; */
+}
+
+.donate-now li {
+  float: left;
+  /* margin: 0 5px 0 0; */
+  width: 100px;
+  height: 40px;
+  /* position: relative; */
+}
+
+.donate-now label,
+.donate-now input {
+  display: block;
+  /* position: absolute; */
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.donate-now input[type="radio"] {
+  opacity: 0.01;
+  z-index: 100;
+}
+
+.donate-now input[type="radio"]:checked+label,
+.Checked+label {
+  background: white;
+  color: black;
+}
+
+.donate-now label {
+  padding: 12px 20px;
+  /* margin: 5px; */
+  border: 1px solid #CCC;
+  cursor: pointer;
+  z-index: 90;
+  text-align: center;
+  font-weight: bold;
+}
+
+.donate-now label:hover {
+  background: #DDD;
+  color: black;
+}
+
+.cont {
+    flex-basis: 100%;
+}
+
+.quantityheader {
+    font-weight: bold;
+    color: #E26565;
+    margin-bottom: 10px;
+}
+
+.quantity-control {
+  flex-basis: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  /* width: fit-content; */
+  /* margin: 0 auto; */
+  background: #eaeaea;
+  border-radius: 3px;
+  padding: 1rem 0.4rem;
+  /* margin-top: 4rem; */
+}
+
+.quantity-btn {
+  background: transparent;
+  border: none;
+  outline: none;
+  margin: 0;
+  padding: 0px 8px;
+  cursor: pointer;
+}
+.quantity-btn svg {
+  width: 15px;
+  height: 15px;
+}
+.quantity-input {
+    color: black;
+  outline: none;
+  user-select: none;
+  text-align: center;
+  width: 47px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  border: none;
+}
+.quantity-input::-webkit-inner-spin-button,
+.quantity-input::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.lastpart {
+    margin-top: 50px;
+    flex-basis: 100%;
+    flex-wrap: wrap;
+    display: flex;
+}
+
+.totaloverall {
+    flex-basis: 100%;
+
+}
+
+.lastpartbuttons {
+    margin-top: 30px;
+    flex-basis: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.addtocart {
+    background-color: #E26565;   
+    padding: 21px 0px;
+    border:0;
+    flex-basis: 90%;
+    cursor: pointer;
+
+}
+
+.favoritebtn {
+    flex-basis: 8%;
+    padding: 20px 0px;
+    border: 1px solid white;
+    background-color: transparent;
+
+}
+
+
+.afterfavoritebtn {
+    flex-basis: 8%;
+    padding: 20px 0px;
+    border: 1px solid white;
+    cursor: pointer;
+    background-color: #8D1D25;
+
+}
+
+
+
+.addreview {
+    margin: 120px 45px;
+    margin-bottom: 30px;
+    
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-between;
+    /* width: 100%; */
+    background-color: rgba(255, 255, 255, 0.3);
+    padding: 20px;
+    border-radius: 3px;
+}
+
+.addareview,.productimageadd {
+    flex-basis: 100%;
+    color: #E26565;
+    margin-bottom: 30px;
+
+}
+
+
+.ratingreviewheader {
+    
+    color: white;
+    font-weight: lighter;
+    flex-basis: 100%;   
+}
+
+.number {
+    flex-basis: 100%;
+    color: black;
+    font-weight: 200;
+    padding: 5px;
+    margin-bottom: 30px;
+}
+
+.addreivewtextarea {
+    height: 100px;
+    background-color: rgba(0, 0, 0, 0.7);
+    border: 0;
+    color: white;
+    padding: 7px;
+    margin-bottom: 30px;
+    border-radius: 5px;
+}
+
+.addreviewbtn {
+    background-color: #E26565;
+    padding: 10px 30px;
+    border: 0;
+    color: black;
+}
+
+.allreviews {
+    margin: 60px 45px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    padding: 20px;
+
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 3px;
+
+    
+}
+
+.numberofreviews {
+    flex-basis: 100%;
+
+
+    border-bottom: 1px solid white;
+    padding-bottom: 10px;
+    margin-bottom: 30px;
+}
+
+.parentsectionreviews {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: flex-start;
+    width: 100%;
+    margin-bottom: 50px;
+
+}
+
+.circle {
+    width: 6vw;
+    height: 6vw;
+    
+    background-color:green;
+    border-radius: 50%;
+    display: inline-block;
+
+}
+
+.parentleft {
+    
+
+}
+
+.parentright {
+    flex-basis: 90%;
+    
+    margin-left: 10px;
+    width: 100%;
+}
+
+
+
+.reviewsrating {
+    display: flex;
+    flex-wrap: wrap;
+    flex-basis: 100%;
+    margin-bottom: 5px;
+    margin-top: 5px;
+}
+
+.reviewdetails {
+    color: white;
+    flex-basis: 100%;
+    margin-bottom: 5px;
+}
+
+.imagescontainer {
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 10px;
+    
+}
+
+
+.picturesreview {
+    width: 5.5vw;
+    height: 5.5vw;
+    background-color: white;
+    margin-right: 10px;
+    
+}
+
+.date {
+    flex-basis: 100%;
+    margin-bottom: 10px;
+}
+
+.like {
+    border-right: 1px solid white;
+    padding-right: 20px;
+    margin-right: 20px;
+    
+}
+
+.replybtn {
+    
+    flex-basis: 100%;
+    color: #E26565;
+    margin-top: 30px;
+    font-size: 1.2em;
+}
+
+
+.usernamereviewcontainer {
+    flex-basis: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.btn {
+    justify-self: flex-end;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* kebab */
+/***
+Bootstrap dropdown container fix
+for Bootstrap 4 and Material Design Bootstrap
+Hint : Move out the button or the link declaring the data-toggle="dropdwn" from the .dropdown div
+Created a dropdown-sm class for smaller dropdowns when using kebabs
+***/
+
+/***
+	Bootstrap dropdown container fix
+	for Bootstrap 4 and Material Design Bootstrap
+	Hint : Move out the button or the link declaring the data-toggle="dropdwn" from the .dropdown div
+	Created a dropdown-sm class for smaller dropdowns when using kebabs
+***/
+
+.dropdown-menu.dropdown-unroll {
+    transition: none;
+}
+
+.kebab-link i {
+    font-size: 1.5rem;
+}
+
+.kebab-link i:active,
+.kebab-link i:hover {
+    color: #4285F4;
+}
+
+
+/*Try to delete this setting and you'll see the problem*/
+
+.kebab-dropdown {
+    position: absolute;
+}
+
+
+/* material design for dropdown */
+
+.dropdown-sm>.dropdown-menu>.dropdown-item {
+    padding: 5px;
+    margin-left: 0;
+}
+
+.dropdown-menu>.dropdown-item {
+    padding: 1rem;
+    margin-left: 0;
+}
+
+.dropdown-menu>.dropdown-item:hover {
+    box-shadow: 0 8px 17px 0 rgba(0, 0, 0, .2), 0 6px 20px 0 rgba(0, 0, 0, .19);
+    color: white;
+    background: #4285F4;
+}
+
+
+/***
+	Bootstrap 4 Collapse Accordion full width clickable
+	from Jacob Lett : http://codepen.io/JacobLett/pen/GWqjPw 
+	with Material Design Bootstrap
+	enhenced by djibe
+	Just add the collapse-accordion class to the div wearing the role="tablist"
+ ***/
+
+.collapse-accordion>.card a[data-toggle=collapse] {
+    display: block;
+    padding: .75rem 1.25rem;
+}
+
+.collapse-accordion>.card a[data-toggle=collapse]:after {
+    /* symbol for "opening" panels */
+    font-family: FontAwesome;
+    font-size: .9rem;
+    content: "\f077";
+    /* adjust as needed, taken from bootstrap.css */
+    float: right;
+    /* adjust as needed */
+    vertical-align: middle;
+    color: grey;
+    /* adjust as needed */
+    transition: all .5s linear;
+    margin-right: 5%;
+}
+
+.collapse-accordion>.card a[data-toggle=collapse].collapsed:after {
+    content: "\f078";
+}
+
+.collapse-accordion>.card>.card-header {
+    padding: 0;
+}
+
+
+.kea {
+    position: relative;
+    
+}
+
+.childreview {
+    margin-left: 6vw;
+}
+
+.childright {
+    margin-left: 10px;
+    /* flex-basis: 100%; */
+    /* width: 100%; */
+    /* display: flex;
+    flex-wrap: wrap; */
+    /* align-self: flex-end; */
+    
+
+}
+/* kebabend */
+</style>
--- test.php
+++ test.php
@@ -1,20 +1,373 @@
 <style>
-    <?php include 'product/css/viewcart.css'; ?>body {
-        background: black !important;
+
+
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+<?php include 'product/css/productcss.php'; ?>
+
+
+
+<style>
+    .rectangle {
+
+        flex-basis: 100%;
+        height: 85vh;
+        <?php
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/swapproj/images/showimage.php';
+        $image = new Image();
+
+        $src = $image->show('uploads/IMG-DEFAULTPROFILE.jpg');
+        echo "background:linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)), url('$src');";
+        ?>background-position: center;
+        background-size: cover;
+        background-image: black;
+
+
+
     }
 </style>
+
+<html>
+
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<div class='top'>
+    <div class='left'>
+        <div class='rectangle'>
+
+        </div>
+
+        <div class="tags">
+            <p class='tagsheader'>Tags</p>
+            <button class='tagsbtn'>Others</button>
+            <button class='tagsbtn'>Utility</button>
+        </div>
+
+        <div class='extrainfo'>
+            <div class='extrainforow'>
+                <span>Delivery Available</span>
+                <i class="tk fas fa-check-circle"></i>
 
 
- 
+            </div>
+
+            <div class='extrainforow'>
+                <span>Fixed Shipping Costs</span>
+                <i class="tk fas fa-check-circle"></i>
+
+
+            </div>
+
+            <div class='extrainforow'>
+                <span>Return/Refund</span>
+                <i class="tk fas fa-times-circle"></i>
+
+
+
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+    <div class="right">
+        <p class='tpamc'>TPAMC</p>
+        <h1 class='prodname'>Portable Torchlight v2</h1>
+
+        <h3 class='price'>$5.99</h3>
+
+        <div class='refunds'>
+            <p class='refundsheader'>No returns/refunds</p>
+            <p class='refundsbody'>Item is confirmed after payment confirmation. No
+                refunds, returns or exchanges will be entertained
+                except as required by law</p>
+
+
+        </div>
+
+
+        <div class="details">
+            <p class="detailsheader">Details</p>
+            <p class='detailsbody'>A flashlight (US, Canada) or torch (UK, Australia) is a portable hand-held electric lamp.
+                Formerly, the light source typically was a miniature incandescent light bulb, but these have
+                been displaced by light-emitting diodes (LEDs) since the mid-2000s. A typical flashlight consists
+                of the light source mounted in a reflector, a transparent cover (sometimes combined with a lens)
+                to protect the light source and reflector, a battery, and a switch, all enclosed in a case.
+            </p>
+        </div>
+
+
+
+        <div class="typescontainer">
+            <div class="typerow">
+                <p class='typename'>Size</p>
+                <div class="variants">
+                    <ul class="donate-now">
+                        <li>
+                            <input type="radio" style='opacity:0' id="a25" name="amount" />
+                            <label for="a25">Small</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="a50" name="amount" />
+                            <label for="a50">Large</label>
+                        </li>
+
+                    </ul>
+
+
+
+
+                </div>
+
+            </div>
+
+            <div class="typerow">
+                <p class='typename'>Weight</p>
+                <div class="variants">
+                    <ul class="donate-now">
+                        <li>
+                            <input type="radio" style='opacity:0' id="5kg" name="weight" />
+                            <label for="5kg">5kg</label>
+                        </li>
+                        <li>
+                            <input type="radio" id="10kg" name="weight" />
+                            <label for="10kg">10kg</label>
+                        </li>
+
+                    </ul>
+
+
+
+
+                </div>
+
+            </div>
+        </div>
+
+
+        <!-- <h1>Quantity İnput</h1> -->
+        <div class='cont'>
+            <p class='quantityheader'>Quantity</p>
+            <div class="quantity-control" data-quantity="">
+                <button class="quantity-btn" data-quantity-minus=""><svg viewBox="0 0 409.6 409.6">
+                        <g>
+                            <g>
+                                <path d="M392.533,187.733H17.067C7.641,187.733,0,195.374,0,204.8s7.641,17.067,17.067,17.067h375.467 c9.426,0,17.067-7.641,17.067-17.067S401.959,187.733,392.533,187.733z" />
+                            </g>
+                        </g>
+                    </svg></button>
+                <input type="number" id='qnn' class="quantity-input" data-quantity-target="" value="1" step="1" min="1" max="10" name="quantity">
+                <button class="quantity-btn" data-quantity-plus=""><svg viewBox="0 0 426.66667 426.66667">
+                        <path d="m405.332031 192h-170.664062v-170.667969c0-11.773437-9.558594-21.332031-21.335938-21.332031-11.773437 0-21.332031 9.558594-21.332031 21.332031v170.667969h-170.667969c-11.773437 0-21.332031 9.558594-21.332031 21.332031 0 11.777344 9.558594 21.335938 21.332031 21.335938h170.667969v170.664062c0 11.777344 9.558594 21.335938 21.332031 21.335938 11.777344 0 21.335938-9.558594 21.335938-21.335938v-170.664062h170.664062c11.777344 0 21.335938-9.558594 21.335938-21.335938 0-11.773437-9.558594-21.332031-21.335938-21.332031zm0 0" />
+                    </svg>
+                </button>
+            </div>
+
+        </div>
+        <p id='qnleft' style='opacity:0.7;margin-top:10px'>Only 3 left</p>
+
+
+        <div class='lastpart'>
+            <h1 class='totaloverall'>Total: $11.98</h1>
+            <div class='lastpartbuttons'>
+                <input class='addtocart' type="submit" value="ADD TO CART">
+                <button class='afterfavoritebtn'><i class="far fa-heart"></i></button>
+            </div>
+
+
+        </div>
 
 
 
 
 
 
+
+
+    </div>
+</div>
+
+
+<div class="addreview">
+    <h3 class='addareview'>Add a review</h3>
+    <div class='ratingreviewadd'>
+        <p class='ratingreviewheader'>Rating</p>
+        <input class='number' type="number" placeholder='1-5'>
+
+    </div>
+
+    <div class='productimageadd'>
+        <p>Product Image</p>
+        <input type='file'>
+    </div>
+
+    <textarea placeholder="I loved this product! I really hope I can get A for SWAP!" class='addreivewtextarea' style="width:100%"></textarea>
+
+    <input type="submit" class='addreviewbtn' value='Post Publicly'>
+
+</div>
+
+
+<div class="allreviews">
+
+
+    <h2 class='numberofreviews'>15 Reviews</h2>
+
+    <div class="parentsectionreviews">
+
+        <div class="parentleft">
+            <div class='circle'>
+
+            </div>
+
+        </div>
+
+        <div class="parentright">
+            <div class='usernamereviewcontainer'>
+                <h4 class='usernamereview'>darrenong</h4>
+
+                <div class='kea'>
+                    <a class="kebab-link" data-toggle="dropdown" data-target="#dropdown1"><i class="fa fa-ellipsis-v"></i></a>
+                    <div class="dropdown kebab-dropdown dropdown-sm" id="dropdown1">
+                        <div class="dropdown-menu dropdown-unroll dropdown-menu-right">
+                            <a class="dropdown-item" href="#">Edit Product</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Delete Product</a>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+
+
+            <div class='reviewsrating'>
+                <i class="fas fa-cog"></i>
+                <i class="fas fa-cog"></i>
+                <i class="fas fa-cog"></i>
+                <i class="fas fa-cog"></i>
+                <i class="fas fa-cog"></i>
+
+
+            </div>
+            <p class='reviewdetails'>Very nice product. makes it portable and stuff.</p>
+
+            <div class="imagescontainer">
+                <div class='picturesreview'>
+
+                </div>
+
+                <div class="picturesreview">
+
+                </div>
+            </div>
+
+            <p class='date'>2021-09-12 20:33</p>
+
+            <div class="likeordislike">
+                <span class='like'>1&nbsp&nbsp<i class="fas fa-chevron-up"></i></span>
+                <span class='dislike'>1&nbsp&nbsp<i class="fas fa-chevron-down"></i></span>
+            </div>
+
+            <p class='replybtn'>Reply</p>
+
+
+        </div>
+
+
+
+    </div>
+
+    <div class='childreview'>
+        <div class="parentsectionreviews">
+
+            <div class="parentleft">
+                <div class='circle'>
+
+                </div>
+
+            </div>
+
+            <div class="childright">
+                <div class='usernamereviewcontainer'>
+                    <h4 class='usernamereview'>darrenong</h4>
+
+                    <div class='kea'>
+                        <a class="kebab-link" data-toggle="dropdown" data-target="#dropdown2"><i class="fa fa-ellipsis-v"></i></a>
+                        <div class="dropdown kebab-dropdown dropdown-sm" id="dropdown2">
+                            <div class="dropdown-menu dropdown-unroll dropdown-menu-right">
+                                <a class="dropdown-item" href="#">Edit Product</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Delete Product</a>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+
+
+
+                <div class='reviewsrating'>
+                    <i class="fas fa-cog"></i>
+                    <i class="fas fa-cog"></i>
+                    <i class="fas fa-cog"></i>
+                    <i class="fas fa-cog"></i>
+                    <i class="fas fa-cog"></i>
+
+
+                </div>
+                <p class='reviewdetails'>Very nice product. makes it portable and stuff.</p>
+
+                <div class="imagescontainer">
+                    <div class='picturesreview'>
+
+                    </div>
+
+                    <div class="picturesreview">
+
+                    </div>
+                </div>
+
+                <p class='date'>2021-09-12 20:33</p>
+
+                <div class="likeordislike">
+                    <span class='like'>1&nbsp&nbsp<i class="fas fa-chevron-up"></i></span>
+                    <span class='dislike'>1&nbsp&nbsp<i class="fas fa-chevron-down"></i></span>
+                </div>
+
+                <p class='replybtn'>Reply</p>
+
+
+            </div>
+
+
+
+
+
+        </div>
+
+    </div>
+
+
+
+
+</div>
+
+</html>
 
 
 
@@ -55,8 +408,79 @@
 
 </head>
 
-<body>
+<script>
+    $(document).ready(function(e) {
+        // Add slideDown animation to Bootstrap dropdown when expanding.
+        $('.dropdown').on('show.bs.dropdown', function() {
+            $(this).find('.dropdown-unroll').first().stop(true, true).slideDown();
+        }).on('hide.bs.dropdown', function() {
+            $(this).find('.dropdown-unroll').first().stop(true, true).slideUp();
+        });
 
-</body>
+    });
+</script>
+
+<script>
+    (function() {
+        "use strict";
+        var jQueryPlugin = (window.jQueryPlugin = function(ident, func) {
+            return function(arg) {
+                if (this.length > 1) {
+                    this.each(function() {
+                        var $this = $(this);
+
+                        if (!$this.data(ident)) {
+                            $this.data(ident, func($this, arg));
+                        }
+                    });
+
+                    return this;
+                } else if (this.length === 1) {
+                    if (!this.data(ident)) {
+                        this.data(ident, func(this, arg));
+                    }
+
+                    return this.data(ident);
+                }
+            };
+        });
+    })();
+
+    (function() {
+        "use strict";
+
+        function Guantity($root) {
+            const element = $root;
+            const quantity = $root.first("data-quantity");
+            const quantity_target = $root.find("[data-quantity-target]");
+            const quantity_minus = $root.find("[data-quantity-minus]");
+            const quantity_plus = $root.find("[data-quantity-plus]");
+            var quantity_ = quantity_target.val();
+            $(quantity_minus).click(function() {
+
+                // value=document.getElementById('qnn').value;
+
+
+                if (parseInt(document.getElementById('qnn').value) > parseInt(document.getElementById('qnn').min)) {
+                    quantity_target.val(--quantity_);
+
+                }
+
+
+
+
+            });
+            $(quantity_plus).click(function() {
+
+                if (parseInt(document.getElementById('qnn').value) < parseInt(document.getElementById('qnn').max)) {
+                    quantity_target.val(++quantity_);
+
+                }
+            });
+        }
+        $.fn.Guantity = jQueryPlugin("Guantity", Guantity);
+        $("[data-quantity]").Guantity();
+    })();
+</script>
 
 </html>
