a4325c9f2c45dcca5b6faecca6b8e499f3c72860
darrenori <87930795+darrenori@users.noreply.github.com>
2022-02-09 09:48:11 +0800

    push

--- router/index.php
+++ router/index.php
@@ -130,7 +130,7 @@ class Router {
     function __destruct() 
     {
         if (!$this->handled) {
-            return include_once(views . '404.html');
+            return include_once(views . '404.php');
         }
     }
 
