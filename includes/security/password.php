28af3b893a4ced7ab06073cd9c581e3cffb807a9
darrenori <darrennorii@gmail.com>
2022-06-18 16:47:00 +0800

    feat(security): add a password strength scorer

---
+++ includes/security/password.php
@@ -0,0 +1,18 @@
+<?php
+
+// Rough password strength score 0..4, for feedback on the signup form.
+function scorePassword(string $pwd): int
+{
+    $score = 0;
+    if (strlen($pwd) >= MIN_PASSWORD_LEN) $score++;
+    if (preg_match('/[A-Z]/', $pwd) && preg_match('/[a-z]/', $pwd)) $score++;
+    if (preg_match('/[0-9]/', $pwd)) $score++;
+    if (preg_match('/[^A-Za-z0-9]/', $pwd)) $score++;
+    return $score;
+}
+
+function passwordLabel(int $score): string
+{
+    $labels = [0 => 'very weak', 1 => 'weak', 2 => 'okay', 3 => 'good', 4 => 'strong'];
+    return $labels[$score] ?? 'weak';
+}
