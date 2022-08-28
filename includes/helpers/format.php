6d517997bd54d6d01ad0f0e06e1498c535428a8f
darrenori <darrennorii@gmail.com>
2022-08-28 15:30:00 +0800

    feat(helpers): add a relative time formatter

--- includes/helpers/format.php
+++ includes/helpers/format.php
@@ -5,3 +5,21 @@ function formatPrice($amount, string $symbol = '$'): string
 {
     return $symbol . number_format((float) $amount, 2);
 }
+
+// "3 days ago" style relative time from a timestamp or date string.
+function timeAgo($when): string
+{
+    $ts = is_numeric($when) ? (int) $when : strtotime((string) $when);
+    $diff = time() - $ts;
+    if ($diff < 60) {
+        return 'just now';
+    }
+    $units = [31536000 => 'year', 2592000 => 'month', 86400 => 'day', 3600 => 'hour', 60 => 'minute'];
+    foreach ($units as $secs => $name) {
+        if ($diff >= $secs) {
+            $n = (int) floor($diff / $secs);
+            return $n . ' ' . $name . ($n > 1 ? 's' : '') . ' ago';
+        }
+    }
+    return 'just now';
+}
