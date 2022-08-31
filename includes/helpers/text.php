77aed30bc1ac295c8accae0006f35d49b623b614
darrenori <darrennorii@gmail.com>
2022-08-31 21:03:00 +0800

    feat(helpers): add a slugify helper for product urls

---
+++ includes/helpers/text.php
@@ -0,0 +1,9 @@
+<?php
+
+// Make a url friendly slug from a product name.
+function slugify(string $text): string
+{
+    $text = strtolower(trim($text));
+    $text = preg_replace('/[^a-z0-9]+/', '-', $text);
+    return trim($text, '-');
+}
