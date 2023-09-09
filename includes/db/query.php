9ca15db0d47fa9ca165de0c5eb28cc351dc3f69f
darrenori <darrennorii@gmail.com>
2023-09-09 20:33:00 +0800

    feat(db): add fetch-one and fetch-all helpers

--- includes/db/query.php
+++ includes/db/query.php
@@ -14,3 +14,17 @@ function dbQuery(mysqli $conn, string $sql, string $types = '', array $params = 
     mysqli_stmt_execute($stmt);
     return $stmt;
 }
+
+// Run a SELECT and get one row, or all rows, as associative arrays.
+function dbFetchOne(mysqli $conn, string $sql, string $types = '', array $params = []): ?array
+{
+    $stmt = dbQuery($conn, $sql, $types, $params);
+    $row = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt));
+    return $row ?: null;
+}
+
+function dbFetchAll(mysqli $conn, string $sql, string $types = '', array $params = []): array
+{
+    $stmt = dbQuery($conn, $sql, $types, $params);
+    return mysqli_fetch_all(mysqli_stmt_get_result($stmt), MYSQLI_ASSOC);
+}
