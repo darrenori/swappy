df2a8878579504b427f086b9ad72f9b8cbbad336
darrenori <darrennorii@gmail.com>
2023-11-18 14:09:00 +0800

    refactor(helpers): add a single include for the helper functions

---
+++ includes/helpers/index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Pull in the helper functions with a single require.
+require_once __DIR__ . '/format.php';
+require_once __DIR__ . '/text.php';
+require_once __DIR__ . '/pagination.php';
+require_once __DIR__ . '/http.php';
